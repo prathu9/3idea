e44f560ebbe9caaeb895b6608028ced0579e6671
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-05-14 23:39:31 +0530

    removed default visible service in services page

--- js/services.js
+++ js/services.js
@@ -2,7 +2,10 @@
     const servicesOptions = document.querySelectorAll(".service-options .option");
     servicesOptions.forEach((option) => {
         option.addEventListener("click", (e) => {
-            document.querySelector(".visible-service").classList.remove('visible-service')
+            const visibleService = document.querySelector(".visible-service");
+            if(visibleService){
+                visibleService.classList.remove('visible-service')
+            }
             document.querySelector(`.${e.currentTarget.dataset.serviceName}`).classList.add("visible-service");
         })
     })

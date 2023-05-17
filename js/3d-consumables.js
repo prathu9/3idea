0fb5bdccba126afd8dfb24707a1c231f4cd0b7e0
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-05-17 13:42:18 +0530

    fixed layout in group page

--- js/3d-consumables.js
+++ js/3d-consumables.js
@@ -1,36 +1,15 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+  slidesPerView: 7,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  
+  pagination: {
+      el: '.swiper-pagination1',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '#js-next4',
+      prevEl: '#js-prev4',
+  },
 });
-
-(function(){
-  const swiper1 = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: '#js-next1',
-        prevEl: '#js-prev1',
-    },
-    breakpoints: {
-        420:{
-            slidesPerView: 2,
-            spaceBetween: 5,
-        },
-        640:{
-            slidesPerView: 3,
-            spaceBetween: 5,
-        },
-        768:{
-            slidesPerView: 4,
-            spaceBetween: 5,
-        },
-        1140: {
-            slidesPerView: 5,
-            spaceBetween: 5,
-        },
-    }
-});
-})()

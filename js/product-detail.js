79108969eadf5fe7793e426adaa9a24f26dd8682
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-07-29 15:25:20 +0530

    added thumbnail and main image in product detail page

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,27 +1,39 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '#js-next',
-        prevEl: '#js-prev',
-    },
-    breakpoints: {
-        250:{
-            slidesPerView: 3,
-            spaceBetween: 10,
+(function(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
-      385:{
-          slidesPerView: 4,
-          spaceBetween: 10,
-      },
-      720:{
-          slidesPerView: 5,
-          spaceBetween: 18,
-      },
-  }
-  });
+        navigation: {
+            nextEl: '#js-next',
+            prevEl: '#js-prev',
+        },
+        breakpoints: {
+            250:{
+                slidesPerView: 3,
+                spaceBetween: 10,
+            },
+          385:{
+              slidesPerView: 4,
+              spaceBetween: 10,
+          },
+          720:{
+              slidesPerView: 5,
+              spaceBetween: 18,
+          },
+      }
+      });
+})();
+
+(function(){
+    const mainImg = document.querySelector(".product-img img");
+    const thumbnailImgs = document.querySelectorAll(".slide-item-wrapper img");
+    thumbnailImgs.forEach((thumbnailImg) => {
+        thumbnailImg.addEventListener("click", (e) =>{
+            mainImg.src = e.currentTarget.src;
+        })
+    })
+})();
